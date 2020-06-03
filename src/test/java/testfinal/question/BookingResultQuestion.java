64e020a4162cee52ae90d5988629dcb8e402bbe8
LeHoa2504 <lehoa543@gmail.com>
2020-06-03 23:58:14 +0700

    first commit

---
+++ src/test/java/testfinal/question/BookingResultQuestion.java
@@ -0,0 +1,22 @@
+package testfinal.question;
+
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Question;
+import net.serenitybdd.screenplay.questions.Text;
+import testfinal.ui.BookingPage;
+
+public class BookingResultQuestion implements Question<String> {
+    public static Question<String> getResult() {
+        return new BookingResultQuestion();
+    }
+
+    @Override
+    public String answeredBy(Actor actor) {
+        String results = Text.of(BookingPage.RESULT)
+                .viewedBy(actor)
+                .asString();
+        String[] result = results.split(" ");
+        return result[2];
+    }
+
+}
---
+++ src/test/java/testfinal/tasks/SearchDestination.java
@@ -0,0 +1,33 @@
+package testfinal.tasks;
+
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Performable;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.actions.Enter;
+import org.openqa.selenium.Keys;
+import testfinal.ui.BookingPage;
+
+import static net.serenitybdd.screenplay.Tasks.instrumented;
+
+public class SearchDestination implements Task {
+    private String destination;
+
+    public SearchDestination(String des){
+        this.destination = des;
+    }
+
+    @Override
+    public <T extends Actor> void performAs(T actor) {
+        actor.attemptsTo(
+                Enter.theValue(destination).into(BookingPage.DES_FIELD),
+                Click.on(BookingPage.OPPTION_DES)
+        );
+    }
+
+    public static Performable withKeyword(String des) {
+        return instrumented(SearchDestination.class, des);
+    }
+
+
+}
